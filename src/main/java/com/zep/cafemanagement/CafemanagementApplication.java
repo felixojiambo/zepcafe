bbea50d6c74ce4501ff7cdd18a2725539a9a9693
Felix Ojiambo <felixojiamboe@gmail.com>
2024-02-03 03:43:22 -0800

    Initial Commit

---
+++ src/main/java/com/zep/cafemanagement/CafemanagementApplication.java
@@ -0,0 +1,14 @@
+package com.zep.cafemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafemanagementApplication.class, args);
+		System.out.println("Test");
+	}
+
+}
